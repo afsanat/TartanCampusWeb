d2cea9cee90601ac0f7f5eb93fb47a9bb3fef083
afsanat <afsanat.ineza@gmail.com>
2024-09-10 01:14:21 +0200

    home page

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,69 @@
+import { Row, Col } from "antd";
+import { withTranslation, TFunction } from "react-i18next";
+import { SvgIcon } from "../../common/SvgIcon";
+import Container from "../../common/Container";
+
+import i18n from "i18next";
+import {
+  Extra,
+  FooterContainer,
+} from "./styles";
+
+interface SocialLinkProps {
+  href: string;
+  src: string;
+}
+
+const Footer = ({ t }: { t: TFunction }) => {
+  const handleChange = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  const SocialLink = ({ href, src }: SocialLinkProps) => {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={src}
+        aria-label={src}
+      >
+        <SvgIcon src={src} width="25px" height="25px" />
+      </a>
+    );
+  };
+
+  return (
+    <>
+      <Extra>
+        <Container border={true}>
+          <Row
+            align="middle"
+            style={{ paddingTop: "3rem" }}
+          >
+            <FooterContainer>
+              <SocialLink
+                href="\"
+                src="github.svg"
+              />
+              <SocialLink
+                href=""
+                src="twitter.svg"
+              />
+              <SocialLink
+                href="\"
+                src="linkedin.svg"
+              />
+              <SocialLink
+                href="\"
+                src="medium.svg"
+              />
+            </FooterContainer>
+          </Row>
+        </Container>
+      </Extra>
+    </>
+  );
+};
+
+export default withTranslation()(Footer);
